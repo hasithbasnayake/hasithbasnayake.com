cf63562e23f97d7d2a0baf2cdf5ac82ca61a3366
hasithbasnayake <131407522+hasithbasnayake@users.noreply.github.com>
2026-08-10 17:28:53 -0700

    Added placeholder post component

---
+++ src/content/posts/test.tsx
@@ -0,0 +1,18 @@
+
+export const metadata = {
+    title: "This is a test post",
+    description: "This is a test description",
+    date: "09.09.09",
+    category: "blog",
+    slug: "this-is-a-test-post",
+};
+
+export default function TestPost () {
+    return(
+        <>
+            <p>I’ve been reading Designing Data-Intensive Applications by Martin Kleppmann & Chris Riccomini and have been inspired to write and build a relational database.
+                A common relational database like the ones queried using SQL is implemented with a B-tree data structure. </p>
+        </>
+
+    );
+}
