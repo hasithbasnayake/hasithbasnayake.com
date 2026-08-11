43ede11bda2de6f2ca4afcd47ec32a31e8c68940
hasithbasnayake <131407522+hasithbasnayake@users.noreply.github.com>
2026-08-11 16:52:32 -0700

    Updated routing

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,7 @@
 import Layout from "./components/layout.tsx";
 import Home from "./pages/home.tsx";
+import Post from "./pages/post.tsx";
+import {PostList} from "./content/index.tsx";
 
 export const routes = [
     {
@@ -7,6 +9,11 @@ export const routes = [
         Component: Layout,
         children: [
             {index: true, Component: Home},
+            {
+                path: ':slug',
+                Component: Post,
+                getStaticPaths: () => PostList.map(post => post.slug),
+            },
         ],
     },
 ];
