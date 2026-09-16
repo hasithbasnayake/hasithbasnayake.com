3721552fe3e2eff78c793b82b3156ed948093a21
hasithbasnayake <131407522+hasithbasnayake@users.noreply.github.com>
2026-09-15 22:52:48 -0700

    Edited post to add video

--- src/content/posts/my-first-post.tsx
+++ src/content/posts/my-first-post.tsx
@@ -1,8 +1,9 @@
 import type {PageFrontmatter} from "../types.ts";
 import styles from "./PostStyles.module.css";
+import ReactPlayer from "react-player";
 
 export const frontmatter: PageFrontmatter = {
-    title: "My First Post",
+    title: "Welcome",
     description: "Welcome to my new portfolio",
     author: "Hasith Basnayake",
     toc: [{
@@ -33,7 +34,17 @@ export default function PostContent () {
                 My old portfolio was hosted on Framer, and the one before that on Webflow. I think my first ever portfolio was a PDF. This is the first time I've attempted to create one fully from scratch. <br></br><br></br>
 
                 It was a cool experience. The site is built entirely with React and Typescript. I got to learn a lot about what goes into building even a simple site.
-                How to think about site architecture, defining shared types, eliminating redundancy, creating a portable design system, and more.  <br></br><br></br>
+                How to think about site architecture, defining shared types, eliminating redundancy, creating a portable design system, and more.
+
+            </p>
+            <div className={styles.playerwrapper}>
+                <ReactPlayer src="https://ujoco4f6nmc9bkmu.public.blob.vercel-storage.com/IMG_8330.mp4" playing={true} muted={true} loop={true} style={{
+                    width: "100%",
+                    height: "100%",
+                }}
+                />
+            </div>
+            <p>
 
                 Now that this site is up, my hope is that I'll be a bit more active on posting my projects and work here. Stay tuned! <br></br><br></br>
 
