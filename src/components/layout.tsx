b0232cf7b5c0fb336ac82cf64f7316ab7f5643c1
hasithbasnayake <131407522+hasithbasnayake@users.noreply.github.com>
2026-09-15 22:53:07 -0700

    Add analytics

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,13 @@
 import {Outlet} from "react-router-dom";
 import styles from "./Layout.module.css";
+import { Analytics } from "@vercel/analytics"
+
 
 export default function Layout() {
     return (
       <div className={styles.page}>
           <Outlet/>
+          <Analytics />
       </div>
     );
 }
